5c8dad2d48af2403146e15b005a15964a7d4fc30
DissanayakeLYB <lasithdissanayake99@gmail.com>
2026-09-16 11:49:31 +0000

    add a fnc to create a matrix

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-	return 0;
+typedef struct {
+	int rows;
+	int columns;
+} matrix;
+
+matrix create_matrix() {
+	
+	matrix matrixA;
+
+	printf("Enter the number of rows: ");
+	scanf("%d", &matrixA.rows);
+	
+	printf("Enter the number of columns: ");
+	scanf("%d", &matrixA.columns);
+
+	printf("Matrix format: (%d,%d)\n", matrixA.rows, matrixA.columns);
+	
+	return matrixA;
 }
+
+int main() {
+
+	matrix matrixA = create_matrix();
+	printf("Matrix: (%d, %d)", matrixA.rows, matrixA.columns);
+	return 0;
+
+};
+
